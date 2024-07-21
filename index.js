2e1bfe8d5dd59dced6bb66dde5705d3d90ac8b16
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2024-07-21 18:54:18 +0000

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { Client, GatewayIntentBits, Collection, Events, Activity, SlashCommandBuilder, Partials } from 'discord.js';
+import { Client, GatewayIntentBits, Collection, Events, Activity, SlashCommandBuilder, Partials} from 'discord.js';
+import fs from 'node:fs';
+import path from 'node:path';
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -10,9 +12,41 @@ const client = new Client({
   ],
   partials: [Partials.Channel, Partials.Message, Partials.User],
 });
-client.on('ready', () => {
+const __dirname = path.resolve();
+async function getPrefixs() {
+  let prefixs = fs.readFileSync(`${__dirname}/prefixs.txt`,"utf-8").split("\n");
+  for (const guilds of client.guilds.cache.values()) {
+      if (!prefixs.find(p => p.startsWith(guilds.id))) {
+        fs.appendFileSync(`${__dirname}/prefixs.txt`, `${guilds.id}: $\n`)
+      } 
+  }
+  prefixs = fs.readFileSync(`${__dirname}/prefixs.txt`,"utf-8").split("\n");
+  console.log(prefixs);
+  return prefixs;
+}
+let prefixList;
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`)
+  prefixList = await getPrefixs();
 })
-
+client.on('messageCreate', (message) => {
+  const prefix = prefixList.find(p => p.startsWith(message.guild.id)).split(": ");
+  if (message.content.toLowerCase().startsWith(`${prefix[1]}setprefix`)) {
+    if (message.member.permissions.has("Administrator")) {
+      const newPrefix = message.content.split(" ")[1];
+      console.log(newPrefix);
+      if (newPrefix) {
+        prefixList = prefixList.filter(p => p.startsWith(message.guild.id));
+        prefixList.set(`${message.guild.id}: ${newPrefix}\n`);
+        fs.writeFileSync(`${__dirname}/prefixs.txt`, prefixList.join(""));
+        message.reply(`Prefix set to ${newPrefix}`);
+      } else {
+        message.reply("Please provide a new prefix");
+      }
+    } else {
+      message.reply("You do not have permission to use this command");
+    }
+  }
+})
 
 client.login(process.env.token)
